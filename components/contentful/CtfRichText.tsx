42449d5580f19c4107817033ace0b1a6076b2d40
Cameron Stewart <babyfacecam@Camerons-Mini.attlocal.net>
2024-06-20 17:16:46 -0500

    fixed build error

--- components/contentful/CtfRichText.tsx
+++ components/contentful/CtfRichText.tsx
@@ -1,7 +1,7 @@
-import {
+/*import {
   documentToReactComponents,
   Options,
-} from "@contentful/rich-text-react-renderer";
+} from "@contentful/rich-text-types";
 import { BLOCKS, Document } from "@contentful/rich-text-types";
 
 import { ArticleImage } from "@src/components/features/article";
@@ -35,7 +35,7 @@ export const contentfulBaseRichTextOptions = ({
   renderNode: {
     [BLOCKS.EMBEDDED_ENTRY]: (node) => {
       const entry = links?.entries?.block?.find(
-        (item: EmbeddedEntryType) => item?.sys?.id === node.data.target.sys.id,
+        (item: EmbeddedEntryType) => item?.sys?.id === node.data.target.sys.id
       );
 
       if (!entry) return null;
@@ -53,4 +53,4 @@ export const CtfRichText = ({ json, links }: ContentfulRichTextInterface) => {
       {documentToReactComponents(json, baseOptions)}
     </article>
   );
-};
+};*/
