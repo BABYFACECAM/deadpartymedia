6e3828addb9e6b56ee72f56770e3e85a27900a4e
Cameron Stewart <babyfacecam@Camerons-Mini.attlocal.net>
2024-06-20 17:39:25 -0500

    fixing client side error

--- utils/contentfulClient.ts
+++ utils/contentfulClient.ts
@@ -1,3 +1,4 @@
+// utils/contentfulClient.ts
 import { createClient } from "contentful";
 
 const {
@@ -5,6 +6,13 @@ const {
   CONTENTFUL_ACCESS_TOKEN,
   CONTENTFUL_PREVIEW_ACCESS_TOKEN,
 } = process.env;
+
+if (!CONTENTFUL_SPACE_ID || !CONTENTFUL_ACCESS_TOKEN) {
+  throw new Error("Contentful space ID or access token is missing.");
+}
+
+console.log("CONTENTFUL_SPACE_ID:", CONTENTFUL_SPACE_ID);
+console.log("CONTENTFUL_ACCESS_TOKEN:", CONTENTFUL_ACCESS_TOKEN);
 
 const client = createClient({
   space: CONTENTFUL_SPACE_ID!,
